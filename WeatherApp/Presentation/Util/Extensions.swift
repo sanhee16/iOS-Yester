cee0201c02507a77b6ff65a53658b9787cba1fa4
sanhee16 <sinhioa20@gmail.com>
2023-07-27 10:07:11 +0900

    📰 Add: UIColor Extension 추가
    - UIColorExtension 추가

--- WeatherApp/Presentation/Util/Extensions.swift
+++ WeatherApp/Presentation/Util/Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Combine
 import Alamofire
+import UIKit
 
 extension String {
     func localizedCountryName() -> String {
@@ -19,7 +20,7 @@ extension String {
             return self
         }
     }
-
+    
     func languageCode() -> String {
         return Bundle.main.preferredLocalizations.first ?? "en"
     }
@@ -42,3 +43,47 @@ extension Publisher {
     }
 }
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+    
+    convenience init(hex: String, opacity: CGFloat = 1.0) {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            self.init(.gray)
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(opacity)
+        )
+    }
+    
+    public static let backgroundColor: UIColor = UIColor(hex: "#F3F6FB")
+    public static let inputBoxColor: UIColor = UIColor(hex: "#DADADA")
+    public static let primeColor1: UIColor = UIColor(hex: "#F07167")
+    public static let primeColor2: UIColor = UIColor(hex: "#F3968F")
+    public static let primeColor3: UIColor = UIColor(hex: "#FECCB7")
+    public static let primeColor4: UIColor = UIColor(hex: "#FFE8D2")
+    public static let primeColor5: UIColor = UIColor(hex: "#FFEAE1")
+    public static let primeColor6: UIColor = UIColor(hex: "#FFF9F9")
+}
---
+++ WeatherApp/Presentation/Util/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  WeatherApp
+//
+//  Created by sandy on 2023/07/27.
+//
+
+import Foundation
+import UIKit
+
+final class Utils {
+    
+}
