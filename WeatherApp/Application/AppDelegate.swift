8c6ecede74c5ab1262865b52855f586b43fc48cb
sanhee16 <sinhioa20@gmail.com>
2023-09-14 19:24:06 +0900

    ✨Feat: Firebase 추가
    - Firebase 추가

--- WeatherApp/Application/AppDelegate.swift
+++ WeatherApp/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 
 @main
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+
         return true
     }
 
