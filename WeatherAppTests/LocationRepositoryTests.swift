ca6c0390174ed30ea0f70b8d210e5890b577a0d9
sanhee16 <sinhioa20@gmail.com>
2023-10-03 13:06:47 +0900

    ✅Test: Unit Test Setting
    - Unit Test Package Setting
    - LocationRepositoryTests 추가

---
+++ WeatherAppTests/LocationRepositoryTests.swift
@@ -0,0 +1,39 @@
+//
+//  LocationRepositoryTests.swift
+//  WeatherAppTests
+//
+//  Created by sandy on 2023/10/02.
+//
+
+import XCTest
+@testable import WeatherApp
+
+final class LocationRepositoryTests: XCTestCase {
+    var sut: LocationRepository! = nil
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = LocationRepository()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
